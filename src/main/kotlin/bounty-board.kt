910b93dc643f752d8e840e876093407c1f633446
DmitriyK <Dmitriykuzminn@gmail.com>
2023-09-29 23:01:32 +0300

    add hero lvl and if\else

--- src/main/kotlin/bounty-board.kt
+++ src/main/kotlin/bounty-board.kt
@@ -1,7 +1,20 @@
 const val HERO_NAME = "Madrigal";
 fun main(){
+    var playerLevel = 1;
     var hasSteed = false;
     var magicMirror = HERO_NAME.reversed()
+    println("Hello $HERO_NAME")
+    println("My lvl is $playerLevel")
+    if (playerLevel == 1) {
+        println("Meet Mr. Bubbles in the land of soft things.")
+    } else {
+        println("Locate the enchanted sword.")
+    }
+    println("Time passes...")
+    println("The hero returns from her quest.")
+    playerLevel++
+    println("Level hero now $playerLevel")
+
 }
 
 
